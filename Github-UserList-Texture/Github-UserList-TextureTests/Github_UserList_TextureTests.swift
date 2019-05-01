6b3353a28ff2d5eb6b7d59f0520a5e974a1f4600
Woopa01 <whdnwls49@naver.com>
2019-05-01 23:29:39 +0900

    initial commit

---
+++ Github-UserList-Texture/Github-UserList-TextureTests/Github_UserList_TextureTests.swift
@@ -0,0 +1,34 @@
+//
+//  Github_UserList_TextureTests.swift
+//  Github-UserList-TextureTests
+//
+//  Created by 조우진 on 01/05/2019.
+//  Copyright © 2019 조우진. All rights reserved.
+//
+
+import XCTest
+@testable import Github_UserList_Texture
+
+class Github_UserList_TextureTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
