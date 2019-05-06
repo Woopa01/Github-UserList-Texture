3efcecac9b0a09348b043b6fd5393d4830d0bfe9
Woopa01 <whdnwls49@naver.com>
2019-05-06 23:56:09 +0900

    [UPDATE] Base
    add findNextLink function

--- Github-UserList-Texture/Github-UserList-Texture/Util/Base.swift
+++ Github-UserList-Texture/Github-UserList-Texture/Util/Base.swift
@@ -17,3 +17,21 @@ extension ASViewController {
         self.navigationController?.navigationBar.setValue(true, forKey: "hidesShadow")
     }
 }
+
+func findNextLink(res: HTTPURLResponse) -> String{
+    var link = ""
+    
+    let allLinkString = res.allHeaderFields["Link"] as? String ?? ""
+    let allLinkArray = allLinkString
+        .filter { $0 != "<" && $0 != ">" }
+        .filter { $0 != ";" && $0 != "," }
+        .split(separator: " ")
+    
+    for i in allLinkArray.indices {
+        if allLinkArray[i] == "rel=\"next\"" {
+            link = String(allLinkArray[i - 1])
+        }
+    }
+    
+    return link
+}
