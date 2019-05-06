bfc9aac640e60b75089fc23c3666118eacc565d7
Woopa01 <whdnwls49@naver.com>
2019-05-06 15:16:17 +0900

    [UPDATE] UserListViewController basic searchRequest list

--- Github-UserList-Texture/Github-UserList-Texture/VC/UserListViewController.swift
+++ Github-UserList-Texture/Github-UserList-Texture/VC/UserListViewController.swift
@@ -9,8 +9,13 @@
 import AsyncDisplayKit
 import RxSwift
 import RxCocoa
+import RxCocoa_Texture
+import RxOptional
 
 class UserListViewController: ASViewController<ASTableNode> {
+    var userlist: [UserModel] = []
+    var viewModel: UserListViewModel!
+    let disposeBag = DisposeBag()
     
     lazy var searchBar: UISearchBar = {
         let bar = UISearchBar()
@@ -24,7 +29,7 @@ class UserListViewController: ASViewController<ASTableNode> {
         tableNode.automaticallyManagesSubnodes = true
         tableNode.backgroundColor = .white
         super.init(node: tableNode)
-
+        
         self.node.onDidLoad { node in
             guard let tableNode = node as? ASTableNode else { return }
             tableNode.view.separatorStyle = .none
@@ -32,8 +37,14 @@ class UserListViewController: ASViewController<ASTableNode> {
         }
         self.node.leadingScreensForBatching = 2.0
         self.node.allowsSelectionDuringEditing = true
+        self.node.delegate = self
+        self.node.dataSource = self
+      
+        bindViewModel()
         
-        
+    }
+    
+    override func viewDidLoad() {
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -41,5 +52,45 @@ class UserListViewController: ASViewController<ASTableNode> {
     }
     
 }
+extension UserListViewController{
+    func bindViewModel(){
+        viewModel = UserListViewModel()
+        
+        searchBar.rx.text
+        .orEmpty
+        .filterEmpty()
+        .distinctUntilChanged()
+        .throttle(0.5, scheduler: MainScheduler.instance)
+        .bind(to: viewModel.searchString)
+        .disposed(by: disposeBag)
+        
+        viewModel.userList.asObservable()
+            .subscribe(onNext: { data in
+                self.userlist = data
+                self.node.reloadData()
+            })
+            .disposed(by: disposeBag)
+        
+    }
+}
 
-
+extension UserListViewController: ASTableDelegate, ASTableDataSource {
+    func numberOfSections(in tableNode: ASTableNode) -> Int {
+        return 1
+    }
+    
+    func tableNode(_ tableNode: ASTableNode, numberOfRowsInSection section: Int) -> Int {
+        return self.userlist.count
+    }
+    
+    func tableNode(_ tableNode: ASTableNode, nodeBlockForRowAt indexPath: IndexPath) -> ASCellNodeBlock {
+        guard self.userlist.count > indexPath.row else { return { ASCellNode() } }
+        
+        let viewModel = UserListCellViewModel(userModel: self.userlist[indexPath.row])
+        
+        return {
+            let cell = UserListCellNode(viewModel: viewModel)
+            return cell
+        }
+    }
+}
