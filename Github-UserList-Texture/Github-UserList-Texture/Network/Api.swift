780c23b4a5d974617ec50516db0355fb7dd8204f
Woopa01 <whdnwls49@naver.com>
2019-05-08 11:17:43 +0900

    [UPDATE] NextLink Variable -> UserDefault

--- Github-UserList-Texture/Github-UserList-Texture/Network/Api.swift
+++ Github-UserList-Texture/Github-UserList-Texture/Network/Api.swift
@@ -37,28 +37,26 @@ class Api: APIProvider{
         return client.get(url: baseURL + Path.searchUser.path(), params: ["q":searchString])
             .map({ res, data -> [UserModel] in
                 
-                self.nextLink = findNextLink(res: res)
-                print("fdsa\(self.nextLink)")
+                UserDefaults.standard.set(findNextLink(res: res), forKey: "nextLink")
                 
                 guard let response = try? JSONDecoder().decode(UserListResponse.self, from: data)
-                else {
-                    print("왜 안돼 ㅜㅜㅜㅜ")
-                    return []
+                    else {
+                        return []
                 }
-                               
+                
                 return response.userlist
             })
         
     }
     
     func loadMoreRequest() -> Observable<[UserModel]> {
-        return client.get(url: self.nextLink, params: nil)
+        return client.get(url: UserDefaults.standard.value(forKey: "nextLink") as! String, params: nil)
             .map({ res, data -> [UserModel] in
-                self.nextLink = findNextLink(res: res)
+                
+                UserDefaults.standard.set(findNextLink(res: res), forKey: "nextLink")
                 
                 guard let response = try? JSONDecoder().decode(UserListResponse.self, from: data)
                     else {
-                        print("이건 또 왜 안돼 ㅜㅜㅜㅜ")
                         return []
                 }
                 
